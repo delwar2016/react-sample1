35e43e82771f910b0c44f366ab6169481468e473
Md. Delwar Hossain <twinkle_023020@yahoo.com>
2016-08-21 11:56:00 +0600

    add express for run server

---
+++ public/app/calculator.js
@@ -0,0 +1,124 @@
+/**
+ * Created by delwar on 8/21/16.
+ */
+
+var CalculatorClass = React.createClass({
+  getInitialState: function() {
+    return { text: 'N/A'};
+  },
+  setNumber1 : function(e){
+    this.number1 = e.target.value;
+    this.setResult();
+  },
+
+  setNumber2 : function(e){
+    this.number2 = e.target.value;
+    this.setResult();
+  },
+
+  operators: ['+', '-','*', '/', '%'],
+
+  setOperator : function(e){
+    this.operator = e.target.value;
+    this.setResult();
+  },
+
+  setResult : function(e){
+    var me = this;
+    var objValid = me.validation();
+    var result = 'N/A';
+
+    if(objValid.isValid){
+      if(e){
+        result = me.calculate();
+      }
+      else{
+        result = objValid.message;
+      }
+
+    }
+    else{
+        result = objValid.message;
+    }
+
+    this.setState({text: result});
+  },
+
+
+  validation: function(){
+
+    if(this.number1 ==='' || !this.number1){
+      return {
+        isValid: false,
+        message: "Please enter number 1"
+      };
+    }
+
+    if(isNaN(this.number1)){
+      return {
+        isValid: false,
+        message: "Please enter valid number"
+      };
+    }
+    if(this.number2 ==='' || !this.number2){
+      return {
+        isValid: false,
+        message: "Please enter number 2"
+      };
+    }
+
+    if(isNaN(this.number2)){
+      return {
+        isValid: false,
+        message: "Please enter valid number"
+      };
+    }
+
+    if(this.operators.indexOf(this.operator) === -1){
+      return {
+        isValid: false,
+        message: "Please enter valid operator"
+      };
+    }
+
+    return {
+      isValid: true,
+      message: "Click on calculate button"
+    }
+  },
+
+
+  calculate: function(){
+    var result = '';
+    if(this.operator ==='+'){
+      result = (Number(this.number1) + Number(this.number2));
+    }
+    if(this.operator ==='-'){
+      result = (Number(this.number1) - Number(this.number2));
+    }
+    if(this.operator ==='*'){
+      result = (Number(this.number1) * Number(this.number2));
+    }
+    if(this.operator ==='/'){
+      result = (Number(this.number1) / Number(this.number2)).toFixed(2);
+    }
+    if(this.operator ==='%'){
+      result = (Number(this.number1) % Number(this.number2));
+    }
+    return result;
+  },
+  render: function() {
+    return  <div>
+    <p>Enter Number 1 <input onChange={this.setNumber1} /></p>
+    <p>Enter Number 2 <input  onChange={this.setNumber2}  /></p>
+    <p>Operator <input  onChange={this.setOperator}  /></p>
+    <p><button onClick={this.setResult} >Calculate</button> <input value={this.state.text} readOnly  /></p>
+    <p></p>
+
+    </div>;
+  }
+});
+
+ReactDOM.render(<CalculatorClass />,  document.getElementById('example'));
+
+
---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by delwar on 8/21/16.
+ */
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+  res.sendFile( __dirname + "/public/view/" + "index.html" );
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
